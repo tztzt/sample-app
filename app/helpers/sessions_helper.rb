913651e462df2bc967838ac35193e43ec42ad3c5
tztzt <zhithonteh@gmail.com>
2021-04-10 16:23:55 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -19,7 +19,9 @@ module SessionsHelper
   def current_user
     if (user_id = session[:user_id])
       user = User.find_by(id: user_id)
-      @current_user ||= user if session[:session_token] == user.session_token
+      if user && session[:session_token] == user.session_token 
+        @current_user ||= user 
+      end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
@@ -46,5 +48,15 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+  
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user && user == current_user
+  end
+  
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
